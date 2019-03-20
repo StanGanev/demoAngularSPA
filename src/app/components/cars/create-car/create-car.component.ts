28ce0286727e36936da08b7f4a4b419b2dbd1750
StanGanev <planbat@abv.bg>
2019-03-20 17:34:13 +0200

    adding new code

--- src/app/components/cars/create-car/create-car.component.ts
+++ src/app/components/cars/create-car/create-car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarModel } from '../model/car.model';
 
 @Component({
   selector: 'app-create-car',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-car.component.css']
 })
 export class CreateCarComponent implements OnInit {
+  car: CarModel
 
-  constructor() { }
+  constructor() {
+    this.car = new CarModel('', '', '', '', 0, '', '', 0, null)
+  }
 
   ngOnInit() {
   }
---
+++ src/app/components/cars/model/car.model.ts
@@ -0,0 +1,13 @@
+export class CarModel {
+    constructor(
+        title: string,
+        description: string,
+        brand: string,
+        model: string,
+        year: number,
+        imageUrl: string,
+        fuelType: string,
+        price: number,
+        sellerId?: string
+    ) {}
+}
